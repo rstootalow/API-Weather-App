ea11ea1a6d8b96a8fabb064041fa033560c018d2
rstutalo <richardtutalo@gmail.com>
2019-07-06 13:13:00 -0600

    weather logic is coded using open weather api. limiting it to city for now but will add state functionality at a later date

---
+++ weather.js
@@ -0,0 +1,26 @@
+// create weather class with constructor
+class Weather {
+    constructor(city, state) {
+        // obfuscated api key since no server side code for application
+        this.apiKey = 'd6b2870c9be0861074154b4b86f1f700';
+        this.city = city;
+        this.state = state;
+    }
+
+    // fetch weather from API
+    async getWeather() {
+        // fetch request to open weather api
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}`);
+
+        // get data from response
+        const responseData = await response.json();
+
+        return responseData;
+    }
+
+    // change weather location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
