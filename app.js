66937f7664597321bcf99172f3e2f00d2cf1d806
rstutalo <richardtutalo@gmail.com>
2019-07-07 15:34:21 -0600

    added some loacal storage configuration but errors exists around getting city state values. push for now but will have to fix before deploying

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+// init Storage class
+const storage = new Storage();
+// get stored location data
+const weatherLocation = storage.getLocationData();
 // init Weather class
-const weather = new Weather('Denver', 'CO'); // hard code current location for a start
-
+const weather = new Weather('Denver', 'CO'); // pass in weatherLocation city/state as params
 //init UI class
 const ui = new UI();
 
 // call these functions when DOM loads
 document.addEventListener('DOMContentLoaded', getWeather);
+
+// change weather location
+document.getElementById('w-change-btn').addEventListener('click', (event) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation(city, state);
+
+    //set location in local storage
+    storage.setLocationData(city, state);
+
+    // call getWeather again to display weather for new location
+    getWeather();
+
+    // close modal after location is updated
+    $('#locModal').modal('hide');
+})
 
 // change location
 // weather.changeLocation('Providence', 'RI');
