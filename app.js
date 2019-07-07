334a0d3e3c26e9d31cdab5b18bcb614cb2fcd41c
rstutalo <richardtutalo@gmail.com>
2019-07-06 23:25:33 -0600

    application is working for general city location as expected. still need styling and will have to set up modal configuration as well

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-// init weather
+// init Weather class
 const weather = new Weather('Denver', 'CO'); // hard code current location for a start
 
-weather.getWeather()
-    .then(results => {
-        console.log(results)
-    })
-    .catch( err => console.log(err))
+//init UI class
+const ui = new UI();
+
+// call these functions when DOM loads
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// change location
+// weather.changeLocation('Providence', 'RI');
+
+function getWeather() {
+    // run get weather function to start on DOM load
+    weather.getWeather()
+        .then(results => {
+            console.log(results)
+            ui.inject(results);
+        })
+        .catch( err => console.log(err))
+}
